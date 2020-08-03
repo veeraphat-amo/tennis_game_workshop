e9ff7103166de51579794164ccbf25ee776efbd1
veeraphat-amo <veeraphat_amo@hotmail.com>
2020-08-03 15:49:18 +0700

    First 3 commits

---
+++ src/main/java/CircularBuffer.java
@@ -0,0 +1,24 @@
+public class CircularBuffer {
+
+    private int bufferSize = 10;
+    private String[] buffer = new String[10];
+    private int readPointer;
+    private int writePointer;
+
+    public boolean isEmpty() {
+        return bufferSize == 10;
+    }
+
+    public void writeData(String input) {
+        bufferSize--;
+        this.buffer[writePointer++] = input;
+    }
+
+    public boolean isFull() {
+        return bufferSize == 0;
+    }
+
+    public String readData() {
+        return this.buffer[readPointer++];
+    }
+}
