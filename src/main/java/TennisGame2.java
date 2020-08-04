a6cd672d79fa5aa1df3dc6cbdae7409eadb6531b
veeraphat-amo <veeraphat_amo@hotmail.com>
2020-08-04 13:51:40 +0700

    Edit main file

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -35,26 +35,26 @@ public class TennisGame2 {
             score = scoreCalled(player1Point);
             score += "-All";
         }
-        else if (player1Point == player2Point && player1Point >= 3) {
+        if (player1Point == player2Point && player1Point >= 3) {
             score = "Deuce";
         }
 
-        else if (player1Point > player2Point && player2Point >= 3) {
+        if (player1Point > player2Point && player2Point >= 3) {
             score = "Advantage player1";
         }
 
-        else if (player2Point > player1Point && player1Point >= 3) {
+        if (player2Point > player1Point && player1Point >= 3) {
             score = "Advantage player2";
         }
 
-        else if (player1Point >= 4 && player2Point >= 0 && (player1Point - player2Point) >= 2) {
+        if (player1Point >= 4 && player2Point >= 0 && (player1Point - player2Point) >= 2) {
             score = "Win for player1";
         }
-        else if (player2Point >= 4 && player1Point >= 0 && (player2Point - player1Point) >= 2) {
+        if (player2Point >= 4 && player1Point >= 0 && (player2Point - player1Point) >= 2) {
             score = "Win for player2";
         }
 
-        else {
+        if (player1Point != player2Point && player1Point<4 && player2Point<4){
             score = scoreCalled(player1Point) + "-" + scoreCalled(player2Point);
         }
         return score;
